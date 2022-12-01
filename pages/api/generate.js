722916eff63a126b0ef159b99dfe1c593505da9c
leo-guinan <leo@copy.ai>
2022-11-30 19:12:06 -0500

    modifying to make project mine

---
+++ pages/api/generate.js
@@ -0,0 +1,27 @@
+import { Configuration, OpenAIApi } from 'openai';
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const basePromptPrefix = "Given the transcript of a podcast episode, write a summary of the episode.\n\nThe transcript:\n";
+const basePromptSuffix = "\n\nSummary:\n";
+const generateAction = async (req, res) => {
+    // Run first prompt
+    console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
+
+    const baseCompletion = await openai.createCompletion({
+        model: 'text-davinci-003',
+        prompt: `${basePromptPrefix}${req.body.userInput}${basePromptSuffix}`,
+        temperature: 0.7,
+        max_tokens: 250,
+    });
+
+    const basePromptOutput = baseCompletion.data.choices.pop();
+
+    res.status(200).json({ output: basePromptOutput });
+};
+
+export default generateAction;
